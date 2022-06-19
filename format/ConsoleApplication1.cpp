e54268018b1881923b746696101823bf41602d44
wangshaohui <wang.shaohui@navercorp.com>
2022-06-19 12:46:29 +0800

    SAVE

--- format/ConsoleApplication1.cpp
+++ format/ConsoleApplication1.cpp
@@ -1,96 +1,24 @@
 ﻿#include <iostream>
 #include <boost/format.hpp>
 
+// C++20提供了source_location，其中存储了full file name, function name, cloumne, row
+
 template<typename... Arguments>
-void format_vargs(std::string const& fmt, Arguments&&... args)
+void format_vargs(std::source_location location, std::string const& fmt, Arguments&&... args)
 {
+        // 注意　这个函数应该放在ｔｒｙ－ｃａｔｃｈ
         boost::format f(fmt);
-         
-        for (const auto &x : std::initializer_list(args)) {
-                f % x ;
-        }
 
-        //int unroll[]{ 0, (f % std::forward<Arguments>(args), 0)... };
-        //static_cast<void>(unroll);
+        int unroll[]{ 0, (f % std::forward<Arguments>(args), 0)... };
+        static_cast<void>(unroll);
 
-        //std::cout << boost::str(f);
+        std::cout << location.file_name() << "  " << location.function_name() << "  " << boost::str(f) << std::endl;
 }
-/*
-void test_format()
+
+
+#define LOG_INFO(fmt, ...) format_vargs(std::source_location::current(), fmt, __VA_ARGS__)
+
+void main()
 {
-        //------------ 1---------------
-        std::cout << boost::format("writing %1%, x=%2% : %3%-th try")
-                % "toto" % 40.23 % 50
-                << std::endl;
-
-        //------------ 2---------------
-        {
-                boost::format f("a=%1%, b=%2%, c=%3%, a=%1%");
-                f % "string" % 2 % 10.0;
-
-                std::cout << f.str() << std::endl;
-        }
-
-        //------------ 3---------------
-        {
-                boost::format f("a=%1%, b=%2%, c=%3%, d=%4%");
-                f % "string";
-                f % 2;
-                f % 10.0;
-                f % "wsh";
-
-                std::cout << f.str() << std::endl;
-        }
-}
-*/
-
-
-class Example {
-private:
-        boost::format *f = nullptr;
-
-        template <typename T>
-        void setVector(T arg) {
-                (*f) % arg;
-        }
-
-        template <typename T, typename... Args>
-        void setVector(T arg, Args... args) {
-                setVector(arg);
-                setVector(std::forward<Args>(args)...);
-        }
-
-public:
-        Example() : f() {}
-        ~Example() {
-                if (f) {
-                        delete f;
-                        f = nullptr;
-                }
-        }
-
-        template <typename T>
-        void callback(T arg) {
-                (*f) % arg;
-        }
-
-        template<typename... Arguments>
-        void format_vargs(std::string const& fmt, Arguments&&... args)
-        {
-                f = new boost::format(fmt);
-                setVector(args...);
-
-                std::cout << boost::str(*f) << '\n';
-
-                delete f;
-                f = nullptr;
-        }
-};
- 
-
-void main(void)
-{
-        Example a;
-        a.format_vargs("This is test for %1%! num:%2%", "WSH", 789);
-        a.format_vargs("Test %s! num:0X%X", "WSH", 789);
+        LOG_INFO("%s %d %d", "Test", 1, 2);
 }
